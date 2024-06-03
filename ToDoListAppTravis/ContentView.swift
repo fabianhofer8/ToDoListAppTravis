0afba178edf15873e4ddd2eef2432d1670de50fa
hofabi <s2010237027@fhooe.at>
2024-06-03 11:17:26 +0200

    Initial Commit

---
+++ ToDoListAppTravis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoListAppTravis
+//
+//  Created by Fabian Hofer on 03.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoListAppTravis/ToDoListAppTravisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppTravisApp.swift
+//  ToDoListAppTravis
+//
+//  Created by Fabian Hofer on 03.06.24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppTravisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
