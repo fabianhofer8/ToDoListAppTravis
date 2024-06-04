b6ccd5e8da2257bebc7233ccdc87f8fe1eda95c3
hofabi <s2010237027@fhooe.at>
2024-06-04 10:02:12 +0200

    add travis file

--- ToDoListAppTravis/ContentView.swift
+++ ToDoListAppTravis/ContentView.swift
@@ -8,17 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var viewModel: ViewModel = ViewModel()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                HStack {
+                    TextField("Enter new task", text: $viewModel.newTask)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Button(action: {
+                        viewModel.addTask()
+                    }) {
+                        Text("Add")
+                    }
+                    .padding(.leading, 8)
+                }
+                .padding()
+
+                List {
+                    ForEach(viewModel.tasks.indices, id: \.self) { index in
+                        HStack {
+                            Text(viewModel.tasks[index].name)
+                            Spacer()
+                            Button(action: {
+                                viewModel.toggleTaskCompletion(at: index)
+                            }) {
+                                Image(systemName: viewModel.tasks[index].isCompleted ? "checkmark.square" : "square")
+                            }
+                        }
+                    }
+                }
+                .navigationBarTitle("ToDo List")
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+class ViewModel: ObservableObject {
+    @Published public var newTask: String = ""
+    @Published public var tasks: [Task] = []
+
+    public func addTask() {
+        guard !newTask.isEmpty else { return }
+        tasks.append(Task(name: newTask))
+        newTask = ""
+    }
+
+    public func toggleTaskCompletion(at index: Int) {
+        tasks[index].isCompleted.toggle()
+    }
+}
+
+struct Task {
+    var name: String
+    var isCompleted: Bool = false
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
--- ToDoListAppTravisTests/ToDoListAppTravisTests.swift
+++ ToDoListAppTravisTests/ToDoListAppTravisTests.swift
@@ -5,32 +5,63 @@
 //  Created by Fabian Hofer on 03.06.24.
 //
 
+
 import XCTest
+import SwiftUI
+import Combine
 @testable import ToDoListAppTravis
 
-final class ToDoListAppTravisTests: XCTestCase {
+class ToDoListAppTravisTests: XCTestCase {
+
+    var viewModel: ViewModel!
+    var cancellables: Set<AnyCancellable> = []
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        viewModel = ViewModel()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        viewModel = nil
+        cancellables = []
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testAddTask() throws {
+        // Given
+        let initialTaskCount = viewModel.tasks.count
+        
+        // When
+        viewModel.newTask = "Test Task"
+        viewModel.addTask()
+        
+        // Then
+        XCTAssertEqual(viewModel.tasks.count, initialTaskCount + 1)
+        XCTAssertEqual(viewModel.tasks.last?.name, "Test Task")
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testToggleTaskCompletion() throws {
+        // Given
+        viewModel.newTask = "Test Task"
+        viewModel.addTask()
+        let taskIndex = viewModel.tasks.count - 1
+        
+        // When
+        let initialCompletionStatus = viewModel.tasks[taskIndex].isCompleted
+        viewModel.toggleTaskCompletion(at: taskIndex)
+        
+        // Then
+        XCTAssertNotEqual(viewModel.tasks[taskIndex].isCompleted, initialCompletionStatus)
     }
 
+    func testAddEmptyTask() throws {
+        // Given
+        let initialTaskCount = viewModel.tasks.count
+        
+        // When
+        viewModel.newTask = ""
+        viewModel.addTask()
+        
+        // Then
+        XCTAssertEqual(viewModel.tasks.count, initialTaskCount)
+    }
 }
+
